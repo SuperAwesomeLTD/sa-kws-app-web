8710c5c14caec5f48987718182790791eeee5470
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-06-16 10:38:11 +0100

    update

--- node.js
+++ node.js
@@ -19,7 +19,7 @@ var kwsSdk = new KwsSdk({
     endpoints: endpoints
 });
 
-app.set('port', (process.env.PORT || 3003));
+app.set('port', (process.env.PORT || 80));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
